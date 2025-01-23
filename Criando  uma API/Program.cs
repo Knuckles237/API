a758b360b210e2473e76f78b4421b6a28d131169
Knuckles237 <jeanagrizzi@gmail.com>
2025-01-22 22:16:17 -0300

    CriandoAPI

--- Criando  uma API/Program.cs
+++ Criando  uma API/Program.cs
@@ -1,6 +1,11 @@
+using WebApplication1.Helpers;
+using WebApplication1.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<TarefaRepositorio>(provider =>
+ new TarefaRepositorio(ConfigurationHelper.GetAppSetting<string>("ConnectionStrings:DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +13,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Registrando o repositório como serviço
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- Criando  uma API/obj/Debug/net8.0/Criando  uma API.AssemblyInfo.cs
+++ Criando  uma API/obj/Debug/net8.0/Criando  uma API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Criando  uma API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c2679b85c13f3f6362f7ad8e15d74e44ad0a48")]
 [assembly: System.Reflection.AssemblyProductAttribute("Criando  uma API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Criando  uma API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
